e955e0da1caf77c387c7e9387c05a33139fb833d
XINXALEGALPRACARAMBA <129397421+XINXALEGALPRACARAMBA@users.noreply.github.com>
2024-05-02 12:07:52 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,154 @@
+//CONST//
+const primeiroNumber1 = 'my name is ' ;
+const segundoNumber1 = 'richardtrs' ;
+
+const operacaoMatematica1 = primeiroNumber1 + 
+segundoNumber1;
+
+
+const primeiroNumber2 = 'eu sou ';
+const segundoNumber2 = 16;
+
+const operacaoMatematica2 = primeiroNumber2 + 
+segundoNumber2;
+
+
+
+const primeiroNumber3 = 1;
+const segundoNumber3 =  1;
+
+const operacaoMatematica3 = primeiroNumber3 + 
+segundoNumber3;
+
+const primeiroNumber4 = 12;
+const segundoNumber4 = 12;
+
+const operacaoMatematica4 = primeiroNumber4 *
+segundoNumber4;
+
+
+const primeiroNumber5 = 1;
+const segundoNumber5 =  1;
+
+
+const operacaoMatematica5 = primeiroNumber5 /
+segundoNumber5;
+
+
+const primeiroNumber6 = 1;
+const segundoNumber6 =  1;
+
+
+const operacaoMatematica6 = primeiroNumber6 -
+segundoNumber6;
+
+//VAR//
+
+var primeiroNumber7 = 1;
+var segundoNumber7 =  1;
+
+var operacaoMatematica7 = primeiroNumber7 + 
+segundoNumber7;
+
+
+var primeiroNumber8 = 1;
+var segundoNumber8 =  1;
+
+var operacaoMatematica8 = primeiroNumber8 -
+segundoNumber8;
+
+
+var primeiroNumber9 = 1;
+var segundoNumber9 =  1;
+
+var operacaoMatematica9 = primeiroNumber9 /
+segundoNumber9;
+
+
+var primeiroNumber10 = 1;
+var segundoNumber10 =  1;
+
+var operacaoMatematica10 = primeiroNumber10 *
+segundoNumber10;
+
+
+var primeiroNumber11 = "dioi soi ";
+var segundoNumber11 =  "ricardo cabrom";
+
+var operacaoMatematica11 = primeiroNumber11 +
+segundoNumber11;
+
+var primeiroNumber12 = "dioi tengo ";
+var segundoNumber12 =  16;
+
+var operacaoMatematica12 = primeiroNumber12 +
+segundoNumber12;
+
+//LET//
+
+let primeiroNumber13 = 1;
+let segundoNumber13 =  1;
+
+let operacaoMatematica13 = primeiroNumber13 + 
+segundoNumber13;
+
+
+let primeiroNumber14 = 1;
+let segundoNumber14 =  1;
+
+let operacaoMatematica14 = primeiroNumber14 -
+segundoNumber14;
+
+
+let primeiroNumber15 = 1;
+let segundoNumber15 =  1;
+
+let operacaoMatematica15 = primeiroNumber15 /
+segundoNumber15;
+
+
+let primeiroNumber16 = 1;
+let segundoNumber16 =  1;
+
+let operacaoMatematica16 = primeiroNumber16 *
+segundoNumber16;
+
+
+let primeiroNumber17 = "Mein Name ist ";
+let segundoNumber17 =  "Richard cabrom";
+
+let operacaoMatematica17 = primeiroNumber17 +
+segundoNumber17;
+
+let primeiroNumber18 = "Ich bin";
+let segundoNumber18 =  16;
+
+let operacaoMatematica18 = primeiroNumber18 +
+segundoNumber18;
+
+
+
+
+//CONST//
+console.log(operacaoMatematica1)
+console.log(operacaoMatematica2)
+console.log(operacaoMatematica3)
+console.log(operacaoMatematica4)
+console.log(operacaoMatematica5)
+console.log(operacaoMatematica6)
+//VAR//
+console.log(operacaoMatematica7)
+console.log(operacaoMatematica8)
+console.log(operacaoMatematica9)
+console.log(operacaoMatematica10)
+console.log(operacaoMatematica11)
+console.log(operacaoMatematica12)
+//LET//
+console.log(operacaoMatematica13)
+console.log(operacaoMatematica14)
+console.log(operacaoMatematica15)
+console.log(operacaoMatematica16)
+console.log(operacaoMatematica17)
+console.log(operacaoMatematica18)
+
+
